e15973a4e32668081713e49ffac4696ce617be13
Matt Krause <matthew.krause@outlook.com>
2021-11-20 23:56:51 -0700

    cowbull working!!

--- 009/cowbull.py
+++ 009/cowbull.py
@@ -6,7 +6,7 @@ def createNumber():
 
 
 def GetInput():
-    guess = input("Enter a 1 digit number: ")
+    guess = input("Guess the 4 digit number: ")
     return guess
 
 
@@ -14,11 +14,12 @@ def check(game_number,guess):
     cowbull = [0,0]
     for i in range(len(game_number)):
         if game_number[i] == guess[i]:
-            cowbull[1]+=1
-        else:
             cowbull[0]+=1
+        else:
+            if game_number.__contains__(guess[i]):
+                cowbull[1]+=1
     return cowbull
-
+1
 
 #Main
 guess_count = 0
@@ -28,10 +29,7 @@ number = 0
 while (number != game_number):
     number = GetInput()
     num_check = check(game_number, number)
-    print(game_number)
-    print(number)
-    print(num_check)
-    # print(check)
+    print(f"Your guess of {number} has {num_check[0]} cows and {num_check[1]} bulls")
     guess_count += 1
 
 print(f"Congratulations, you guessed the number. It took you {guess_count} guesses!")
