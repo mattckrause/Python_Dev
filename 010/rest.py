48820a42b2cdb35cf4a3736cfecad55065b923d7
Matt Krause <matthew.krause@outlook.com>
2021-11-21 21:31:47 -0700

    made a Graph API rest request work!... it's ugly

---
+++ 010/rest.py
@@ -0,0 +1,8 @@
+﻿import requests
+import json
+access_token = ""
+
+
+response = json.loads(requests.get('https://graph.microsoft.com/v1.0/users/',headers={"Authorization": "Bearer "+access_token}).text)
+
+print(response)
