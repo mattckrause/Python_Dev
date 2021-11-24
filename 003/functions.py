d7d12d12c9b90b2052db8b382831412f6de8cad6
Matt Krause <matthew.krause@outlook.com>
2021-11-24 14:52:52 -0700

    clean up code in new IDE

--- 003/functions.py
+++ 003/functions.py
@@ -1,28 +1,23 @@
-﻿#Functions/loops/random/file output - day 3
+
+# Functions/loops/random/file output - day 3
 
 
 import random
-import numpy
-
 
 array = []
 file = "C:/temp/pytest.txt"
-#functions
-def func(a,b):
+# functions
+
+
+def func(a, b):
     while a <= b:
-        array.append(random.randint(1,100000))
-        a=a+1
+        array.append(random.randint(1, 100000))
+        a = a+1
     return
 
 
-func(1,1000)
+func(1, 1000)
 sort = sorted(array)
-#outdata = " ".join(sort)
+
 print(f"writing data to {file}")
 print(sort)
-
-#print(outdata)
-
-#with open(file, 'w') as Outfile:
-#   for line in outdata:
-#        Outfile.write(" ".join(line) + "\n") 
---
+++ 011/GraphApp/auth_helper.py
@@ -0,0 +1,14 @@
+﻿import json
+import msal
+import os
+
+
+# Load the oauth_settings.yml file
+settings = json.load(open('C:/dev/Python_Dev/011/GraphApp/auth_config.json', 'r'))
+
+
+
+# print(stream)
+# settings = json.load(stream)
+
+print(settings)
